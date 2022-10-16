804d24715a26d947bb31d8b5d30735870be1f79c
James Espinosa <de.james013@gmail.com>
2022-10-16 23:15:10 +0800

    as

--- scratch.c
+++ scratch.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#include <string.h>
-#include <windows.h>
-#include <math.h>
 
 int main()
 {
-    char a = 'a';
-    char x = '1';
-    char new_a = toupper(x);
-    printf("%c\n", new_a);
+    printf("I FCKING WROTE THIS SHIT\n");
     return 0;
 }
